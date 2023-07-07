92f8a196947bf6516da2dcd50f56960790defc33
Bayron A. Picado Obando <bypiob@gmail.com>
2023-07-06 19:24:54 -0600

    Adding Firebase
    Revert "Adding Firebase"
    
    This reverts commit b1a82413fb9cb054ff61b16e4920a92f943ab482.
    
    Test
    
    Test2
    
    Update yml
    
    Test3
    
    Test4

--- src/pages/Visualizer/Visualizer.tsx
+++ src/pages/Visualizer/Visualizer.tsx
@@ -13,7 +13,7 @@ export const Visualizer = () => {
       greenColor={greenColor}>
         <button onClick={() => setGreenColor(!greenColor)}>CHANGE COLOR</button>
         <p>
-          Visualizer Page
+          Visualizer Page, now on Firebase
         </p>     
 
     </StyledDiv>
