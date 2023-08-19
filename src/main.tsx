7e29e03981a4014bc4e2085d232183029f7bf93b
Bayron A. Picado Obando <bypiob@gmail.com>
2023-08-18 18:07:33 -0600

    Fix issue with build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from './components'
-import { Main } from './pages/Main/Main'
 import { Home } from './pages/Home/Home'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
