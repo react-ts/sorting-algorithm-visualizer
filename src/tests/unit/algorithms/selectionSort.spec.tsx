e2d3cf4fa51b62a6d3ed64d70341c508dd43d930
Oscar Castro <ocastror24@gmail.com>
2023-09-12 23:48:07 -0600

    selectionSort implemented

---
+++ src/tests/unit/algorithms/selectionSort.spec.tsx
@@ -0,0 +1,50 @@
+import { describe, expect, test } from "vitest";
+import { selectionSort } from "../../../algorithms";
+import { IMovement } from "../../../interfaces";
+
+describe("selectionSort tests", () => {
+  test("should return an array of IMovement", () => {
+    const array = [5, 3, 4, 2, 1, 0, 7];
+    const mockMovements = [
+      { indexA: 1, indexB: 1, swap: false },
+      { indexA: 1, indexB: 0, swap: true },
+      { indexA: 2, indexB: 2, swap: false },
+      { indexA: 2, indexB: 0, swap: true },
+      { indexA: 3, indexB: 3, swap: false },
+      { indexA: 3, indexB: 0, swap: true },
+      { indexA: 4, indexB: 4, swap: false },
+      { indexA: 4, indexB: 0, swap: true },
+      { indexA: 5, indexB: 5, swap: false },
+      { indexA: 5, indexB: 0, swap: true },
+      { indexA: 5, indexB: 0, swap: true },
+      { indexA: 2, indexB: 2, swap: false },
+      { indexA: 2, indexB: 1, swap: true },
+      { indexA: 3, indexB: 3, swap: false },
+      { indexA: 3, indexB: 1, swap: true },
+      { indexA: 4, indexB: 4, swap: false },
+      { indexA: 4, indexB: 1, swap: true },
+      { indexA: 5, indexB: 5, swap: false },
+      { indexA: 5, indexB: 1, swap: true },
+      { indexA: 5, indexB: 1, swap: true },
+      { indexA: 3, indexB: 3, swap: false },
+      { indexA: 3, indexB: 2, swap: true },
+      { indexA: 4, indexB: 4, swap: false },
+      { indexA: 4, indexB: 2, swap: true },
+      { indexA: 5, indexB: 5, swap: false },
+      { indexA: 5, indexB: 2, swap: true },
+      { indexA: 5, indexB: 2, swap: true },
+      { indexA: 4, indexB: 4, swap: false },
+      { indexA: 4, indexB: 3, swap: true },
+      { indexA: 5, indexB: 5, swap: false },
+      { indexA: 5, indexB: 3, swap: true },
+      { indexA: 5, indexB: 3, swap: true },
+      { indexA: 5, indexB: 5, swap: false },
+      { indexA: 5, indexB: 4, swap: true },
+      { indexA: 5, indexB: 4, swap: true },
+      { indexA: 5, indexB: 5, swap: true }
+    ];
+    const movements = selectionSort(array);
+    expect(movements).toBeInstanceOf<IMovement[]>
+    expect(movements).toStrictEqual(mockMovements)
+  });
+});
